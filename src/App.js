396912f132ebdf3d14cb579ecea742cc9b15657c
SeanKim <sean.kim84@gmail.com>
2018-09-22 10:30:17 +0700

    Fixing the Projects

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends Component {
           <div className="banner">
             <div className="banner-center">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png" alt="React_Logo"/>
-              <h1>About Sean<span clanssName="and"> &amp;
-              </span>co</h1>
-              <h3>Intro of MySelf to Friends</h3>
+              <h1>Welcome to Sean's Space<span clanssName="and"> 
+              </span></h1>
+              <h3>Where there's a will, there's a way</h3>
               <div className="banner-icons">
                 <a href=""><i class="fab fa-pinterest-square"></i></a>
                 <a href=""><i class="fab fa-facebook-square"></i></a>
@@ -74,60 +74,64 @@ class App extends Component {
         </section>
 
         <section className="projects">
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://cdn-images-1.medium.com/max/679/1*qUlxDdY3T-rDtJ4LhLGkEg.png) center/cover "}}>React</div>
+            <div className="card-bottom">
+              <div className="card-text"> Movie_Web</div>
+              <div className="btn">Get Started</div>
+            </div>
+            
           </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://cdn-images-1.medium.com/max/2000/1*_DOHv30w-0eI-Ysz5U47Yg.png" alt="" />
-            </article>
+          <div className="card">
+            <div className="card-title" style={{background:"url(https://cdn-images-1.medium.com/max/1600/1*DVkLFr953djSo0q6cA0-kg.png) center/cover "}}>React_Native</div>
+            <div className="card-text">Weather_App</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://s3.amazonaws.com/clarityfm-production/attachments/6605/default/django.png?1442839704) center/cover "}}>Django</div>
+            <div className="card-text">sadfljsldkfjlsakjdflkjsadflksldfjkslfdjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://cdn-images-1.medium.com/max/1600/1*safAvjgR68qpQCreDTOcYA.png" alt="" />
-            </article>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_1489714904/typescript.png) center/cover "}}>TypeScript</div>
+            <div className="card-text">sadfljslddjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://camo.githubusercontent.com/011f821efc77f28ac981c7804b92ad2ffa93cfc1/68747470733a2f2f7777772e7265696e6465782e696f2f626c6f672f6275696c64696e672d612d6772617068716c2d7365727665722d776974682d6e6f64652d6a732d616e642d73716c2f4772617068514c5f4e6f64654a532e706e67) center/cover "}}>Apollo/GraphQL</div>
+            <div className="card-text">sadfljsldkfjlsakjdflkjsadflksldfjkslfdjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://discoversdkcdn.azureedge.net/postscontent/Django.jpg" alt="" />
-            </article>
+          <div className="card">
+            <div className="card-title" style={{background:"url(http://professional-guru.com/wp-content/uploads/2017/12/Blockchain-Logo-White-1.png) center/cover "}}>BlockChain</div>
+            <div className="card-text">sadfljsldkfjlsakjdflkjsadflksldfjkslfdjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png) center/cover "}}>Redux</div>
+            <div className="card-text">sadfljsldkfjlsakjdflkjsadflksldfjkslfdjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://cdn-images-1.medium.com/max/1600/1*Wvsl4OLL8kcZD4Qo6uqDuw.png" alt="" />
-            </article>
+          <div className="card">
+            <div className="card-title" style={{ background:"url(https://www.datocms-assets.com/205/1516875064-next.svg) center/cover "}}>NEXT.JS</div>
+            <div className="card-text">sadfljsldkfjlsakjdflkjsadflksldfjkslfdjlsakdfjlskj</div>
+            <div className="card-action">
+              <button>Get Start</button>
+            </div>
           </div>
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
-          </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://t4.ftcdn.net/jpg/02/04/17/49/240_F_204174960_1udPCiS0nBe4NpH5niBN8x54jLhe6Nzy.jpg" alt="" />
-            </article>
-          </div>
-          <div className="title">
-            <h2>Projects</h2>
-            <div className="title-underline"></div>
-          </div>
-          <div className="projects-center">
-            <article className="project">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVbPnYv4hogHgkGsDOaFYX5weDqM3FOecsfAtGzVCIt9r1wsGN" alt="" />
-            </article>
-          </div>
+               
         </section>
 
         <section className="about">
